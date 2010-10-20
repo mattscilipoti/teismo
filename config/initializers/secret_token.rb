d706fd4bb3dd68861f529e8067187a31e16dbcf4
Matt Scilipoti <matt@scilipoti.name>
2010-10-19 22:30:58 -0400

    rails new theism --skip-prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theism::Application.config.secret_token = '55c835e314947b101432a68bd53476ac9a5e76f1dc44efc093675d86b13d929ace95b94fd23c4280d31d4c565ebec7dc406f39130f00858dbe564743acf50e1d'
