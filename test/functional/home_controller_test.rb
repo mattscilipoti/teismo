46eae93ca834c7356fc19b54195ee2fe073433fc
Matt Scilipoti <matt@scilipoti.name>
2010-10-21 00:24:54 -0400

    First home page.

---
+++ test/functional/home_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+require 'minitest/spec'
+MiniTest::Unit.autorun
+
+describe HomeController do
+  describe 'home' do
+    it "should route '/' to home#show"
+      # root_path.should == 'home'
+    # end
+  end
+end
